ee3e51022bb83a0f506db62574dae84fb416f17c
simpleuser123456 <kkurajj@gmail.com>
2024-05-11 20:00:47 +1000

    delete unused from main

--- toy_shop/src/main/java/com/toy_shop/Main.java
+++ toy_shop/src/main/java/com/toy_shop/Main.java
@@ -2,10 +2,6 @@ package com.toy_shop;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
         ToyStorage shop = new ToyStorage("1 2 конструктор", "2 2 робот", "3 6 кукла");
-
-        System.out.println(shop.get());
     }
 }
